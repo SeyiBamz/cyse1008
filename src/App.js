d62fde9bf1080767a4d862e15c658d70db0c77b7
unknown <Ayobami Olanrewaju@UCAP-IT-26.ubacapitalgroup.com>
2024-02-06 09:48:28 -0500

    Assignment one

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello my Love
         </a>
       </header>
     </div>
